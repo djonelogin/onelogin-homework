a73d2e974ad864fb842ae9e435290b555eb4a8c2
Danye Jia <danye.jia@morningstar.com>
2021-04-17 17:23:36 -0500

    --wip-- [skip ci]

--- src/main/java/org/dj/onelogin/Fraction.java
+++ src/main/java/org/dj/onelogin/Fraction.java
@@ -1,16 +1,103 @@
 package org.dj.onelogin;
 
-public class Fraction {
-    public int val;
+import java.util.Objects;
 
-    public Fraction(int val) {
-        this.val = val;
+public class Fraction {
+
+    public static Fraction ZERO = new Fraction(0, 0, 1);
+
+    public String str;
+    public int whole;
+    public int numerator;
+    public int denominator;
+
+    public Fraction(String str) {
+        this.str = str;
+    }
+
+    public Fraction(int whole, int numerator, int denominator) {
+        this.whole = whole;
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public static Fraction from(int numerator, int denominator) {
+        int whole = numerator / denominator;
+        numerator %= denominator;
+
+        int gcd = getGCD(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+        if (numerator % denominator == 0) {
+            denominator = 1;
+        }
+
+        return new Fraction(whole, numerator, denominator);
+    }
+
+    private static int getGCD(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return getGCD(b % a, a);
+    }
+
+    private int getLCM(int a, int b) {
+        return (a / getGCD(a, b)) * b;
+    }
+
+    public Fraction add(Fraction other) {
+        int lcm = getLCM(this.denominator, other.denominator);
+        int num = lcm / this.denominator * (this.whole * this.denominator + this.numerator)
+                +
+                lcm / other.denominator * (other.whole * other.denominator + other.numerator);
+        int denom = lcm;
+        return Fraction.from(num, denom);
+    }
+
+    public Fraction sub(Fraction other) {
+        int lcm = getLCM(this.denominator, other.denominator);
+        int num = lcm / this.denominator * (this.whole * this.denominator + this.numerator)
+                -
+                lcm / other.denominator * (other.whole * other.denominator + other.numerator);
+        int denom = lcm;
+        return Fraction.from(num, denom);
+    }
+
+    public Fraction mul(Fraction other) {
+        int num = (this.whole * this.denominator + this.numerator)
+                *
+                (other.whole * other.denominator + other.numerator);
+        int denom = this.denominator * other.denominator;
+        return Fraction.from(num, denom);
+    }
+
+    public Fraction div(Fraction other) {
+        int num = other.denominator * (this.whole * this.denominator + this.numerator);
+        int denom = this.denominator * (other.whole * other.denominator + other.numerator);
+        return Fraction.from(num, denom);
     }
 
     @Override
     public String toString() {
         return "Fraction{" +
-                "val=" + val +
+                "str='" + str + '\'' +
+                ", whole=" + whole +
+                ", numerator=" + numerator +
+                ", denominator=" + denominator +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return whole == fraction.whole && numerator == fraction.numerator && denominator == fraction.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(whole, numerator, denominator);
     }
 }
--- src/main/java/org/dj/onelogin/Homework.java
+++ src/main/java/org/dj/onelogin/Homework.java
@@ -8,13 +8,26 @@ import java.util.Map;
 public class Homework {
     public static void main(String[] args) {
         Homework homework = new Homework();
-        int result;
-        result = homework.calculateNum("3+5+2*2");
-        System.out.println(result);
 
-//        Fraction res;
-//        res = homework.calculate("any");
-//        System.out.println(res);
+//        test(homework);
+    }
+
+    private static void test(Homework homework) {
+        Fraction res;
+        res = homework.calculateNum(" 1/2 + 3/4  ");
+        assert(new Fraction(1, 1, 4).equals(res));
+        res = homework.calculateNum("3_1/2 + 1/2");
+        assert(new Fraction(4, 0, 1).equals(res));
+        res = homework.calculateNum("-1/2 + 2/3");
+        assert(new Fraction(0, 1, 6).equals(res));
+        res = homework.calculateNum("1/2 * 3_3/4");
+        assert(new Fraction(1, 7, 8).equals(res));
+        res = homework.calculateNum("2_3/8 + 9/8");
+        assert(new Fraction(3, 1, 2).equals(res));
+        res = homework.calculateNum("1/2 / 3/8");
+        assert(new Fraction(1, 1, 3).equals(res));
+        res = homework.calculateNum("3_1/2 / 2_4/9");
+        assert(new Fraction(1, 19, 44).equals(res));
     }
 
     Map<Character, Integer> procedenceMap = new HashMap<Character, Integer>() {{
@@ -24,27 +37,29 @@ public class Homework {
         put('/', 2);
     }};
 
-    public int calculateNum(String s) {
+    public Fraction calculateNum(String s) {
+        s = s.trim();
         s.replace("\\(-", "(0-");
         s.replace("\\(\\+", "(0+");
 
         char[] cs = s.toCharArray();
         int n = s.length();
-        Deque<Integer> numStack = new LinkedList<>();
-        numStack.addLast(0);
+        Deque<Fraction> numStack = new LinkedList<>();
+        numStack.push(Fraction.ZERO);
         Deque<Character> opStack = new LinkedList<>();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i >= 0 && i < n; i++) {
             char c = cs[i];
             if (Character.isDigit(c)) {
-                int u = 0;
-                int j = i;
-                while (j < n && Character.isDigit(cs[j])) {
-                    u = u * 10 + (cs[j] - '0');
-                    j++;
+                int end = s.indexOf(' ', i);
+                if (end == -1) {
+                    end = n;
                 }
-                numStack.push(u);
-                i = j - 1;
+                Fraction fr = parseFraction(cs, i, end);
+                numStack.push(fr);
+                i = end - 1;
+            } else if (c == ' ') {
+                continue;
             } else {
                 while (!opStack.isEmpty()) {
                     char prevOp = opStack.peek();
@@ -65,27 +80,57 @@ public class Homework {
         return numStack.peek();
     }
 
-    private void calc(Deque<Integer> numStack, Deque<Character> opStack) {
+    private Fraction parseFraction(String s) {
+        return parseFraction(s.toCharArray(), 0, s.length());
+    }
+
+    private Fraction parseFraction(char[] cs, int start, int end) {
+        int whole = 0, numerator = 0, denominator = 0;
+        int num = 0;
+        boolean isFraction = false;
+        for (int i = start; i < end; i++) {
+            char c = cs[i];
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '/') {
+                isFraction = true;
+
+                numerator = num;
+                num = 0;
+            } else if (c == '_') {
+                whole = num;
+                num = 0;
+            }
+        }
+
+        if (isFraction) {
+            denominator = num;
+        } else {
+            whole = num;
+            numerator = 0;
+            denominator = 1;
+        }
+
+        return new Fraction(whole, numerator, denominator);
+    }
+
+    private void calc(Deque<Fraction> numStack, Deque<Character> opStack) {
         if (opStack.isEmpty() || numStack.isEmpty() || numStack.size() < 2) {
             return;
         }
-        int b = numStack.pop(), a = numStack.pop();
+        Fraction b = numStack.pop(), a = numStack.pop();
         char op = opStack.pop();
 
-        int ans = 0;
+        Fraction ans = null;
         if (op == '+') {
-            ans = a + b;
+            ans = a.add(b);
         } else if (op == '-') {
-            ans = a - b;
+            ans = a.sub(b);
         } else if (op == '*') {
-            ans = a * b;
+            ans = a.mul(b);
         } else if (op == '/') {
-            ans = a / b;
+            ans = a.div(b);
         }
         numStack.push(ans);
-    }
-
-    public Fraction calculate(String input) {
-        return new Fraction(3);
     }
 }
